f364b642f88e0e0ca2f89a4c06f9da2841d595f3
Tomasz Tarabasz <tomektarabasz@gmail.com>
2021-02-19 00:23:38 +0100

    Changes in cityApi and Home

--- apps/court-app/src/app/api/cityApi.ts
+++ apps/court-app/src/app/api/cityApi.ts
@@ -1,10 +1,20 @@
 import axios from "axios"
 
-export const getCities = () => {
+export type City = {
+  _id:string,
+  name: string,
+  key: string,
+  wojewodztwo: string,
+  powiat: string,
+  gmina: string,
+}
+
+export const getCities = (queryCity,callback) => {
+    console.log(queryCity)
     axios.get("http://0.0.0.0:80/all",{
           }).then(
             (response)=>{
-              console.log({response})
+              callback(response.data)
             }
           )
 }
--- apps/court-app/src/app/pages/Home/Home.tsx
+++ apps/court-app/src/app/pages/Home/Home.tsx
@@ -1,45 +1,43 @@
-import React from 'react';
-import {
-    FilledInput,
-    FormControl,
-    InputLabel,
-  } from '@material-ui/core';
+import React, { useState } from 'react';
+import { FilledInput, FormControl, InputLabel } from '@material-ui/core';
 import clsx from 'clsx';
 import { Background, useStyles } from './Home.styled';
-import { getCities } from "@api/cityApi";
+import { City, getCities } from '@api/cityApi';
 
 export const Home: React.FC = () => {
   const classes = useStyles();
-  const handleBlur = () => {
-    console.log("asdsdf")
+  const [cities, setCities] = useState([] as City[]);
+  const handleBlur = (event) => {
+    getCities(event.target.value,setCities);
   };
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
-      if(event.key === "Enter") {
-          alert("evet.key")
-          getCities()
-      }
-  }
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      getCities(event.target.value,setCities);
+    }
+  };
   return (
-      <Background>
-        <div className={classes.cities}>
-          <FormControl
-            className={clsx(
-              classes.cities,
-              classes.textField,
-              classes.inputBackground
-            )}
-            variant="filled"
-          >
-            <InputLabel htmlFor="city-input">Miasto</InputLabel>
-            <FilledInput
-              id="city-input"
-              type={'text'}
-              // value={values.password}
-              onBlur={handleBlur}
-              onKeyPress={handleKeyPress}
-            />
-          </FormControl>
-        </div>
-      </Background>
+    <Background>
+      <div className={classes.cities}>
+        <FormControl
+          className={clsx(
+            classes.cities,
+            classes.textField,
+            classes.inputBackground
+          )}
+          variant="filled"
+        >
+          <InputLabel htmlFor="city-input">Miasto</InputLabel>
+          <FilledInput
+            id="city-input"
+            type={'text'}
+            onBlur={handleBlur}
+            onKeyPress={handleKeyPress}
+          />
+        </FormControl>
+        {cities && cities.map(
+            (city) => <div>{city.name}</div>
+          )}
+      </div>
+    </Background>
   );
 };
